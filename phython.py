c40e226fcdf63692c997b7bd8754bb7309f5ecba
leoseiji13 <119980191+leoseiji13@users.noreply.github.com>
2022-12-07 20:41:49 -0300

    Add files via upload

---
+++ phython.py
@@ -0,0 +1,39 @@
+
+
+
+
+
+
+print('Bem-vindo a agenda eletrônica')
+p1 = str(input('Digite seu primeiro produto:'))
+p2 = str(input('Digite seu segundo produto:'))
+p3 = str(input('Digite seu terceiro produto:'))
+
+
+print('Usuário, você deve comprar os seguintes itens:')
+print(p1,p2,p3)
+
+    
+
+
+
+
+
+
+
+
+                 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
